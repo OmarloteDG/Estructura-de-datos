93c2a2bce8d98890913575521ce3e7970381dd88
daniellote <daniellotegarcia@hotmail.com>
2025-03-17 11:33:37 -0500

    parcialpractico1

--- src/Main.java
+++ src/Main.java
@@ -1,102 +1,95 @@
-import java.util.Scanner;   /// daniel lote garcia
+import java.util.Random;   ///// daniel lote garcia
+import java.util.Scanner;  //// PARIAL PRACTICO
+
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner teclado = new Scanner(System.in);
-        int opcion, cantidad = 0;
-        String nombre, fecha, tel, producto = "";
-        double totalCompra = 0, precioUnitario = 0, subtotal = 0, iva = 0 , totalFactura = 0;
+        Random ale = new Random();
+        int max = 12;
+        int min = 1;
 
-        System.out.print("Ingrese su nombre: ");
-        nombre = teclado.nextLine();
+        int vida = 15;
+        int experiencia = 0;
+        boolean envenenado = false;
 
-        System.out.print("Ingrese la fecha: ");
-        fecha = teclado.nextLine();
+        System.out.print("VIENVENIDO AL JUEGO");
+        while (vida > 0 && experiencia < 50) {
 
-        System.out.print("Ingrese su teléfono: ");
-        tel = teclado.nextLine();
+            System.out.println("Presiona Enter para lanzar el dado...");
+            teclado.nextLine();
+            int num = ale.nextInt((max - min + 1)) + min;
+            System.out.println("Número obtenido: " + num);
 
-        do {
-            System.out.println("MENU DE OPCIONES");
-            System.out.println("1. MAUSE 80.000");
-            System.out.println("2. TECLADO 320.000");
-            System.out.println("3. MONITOR 940.000");
-            System.out.println("4. DISCO DURO 230.000");
-            System.out.println("5. RAM 180.000");
-            System.out.println("6. SALIR");
-            System.out.print("Seleccione una opción: ");
-            opcion = teclado.nextInt();
-
-            switch (opcion) {
-                case 1 -> {
-                    producto = "Mause";
-                    precioUnitario = 80000;
+            switch (num) {
+                case 1, 2 -> {
+                    System.out.print("Encuentras una fuente de vida ");
+                    vida += 1;
+                    System.out.println("Vida: " + vida + " | Experiencia: " + experiencia);
                 }
-                case 2 -> {
-                    producto = "Teclado";
-                    precioUnitario = 320000;
+
+                case 3, 5 -> {
+                    System.out.print("Eres atacado brutalmente");
+                    vida -= 5;
+                    System.out.println("Vida: " + vida + " | Experiencia: " + experiencia);
                 }
-                case 3 -> {
-                    producto = "Monitor";
-                    precioUnitario = 940000;
-                }
+
                 case 4 -> {
-                    producto = "Disco Duro";
-                    precioUnitario = 230000;
+                    System.out.print("NO OBTIENES NADA");
                 }
-                case 5 -> {
-                    producto = "Ram";
-                    precioUnitario = 180000;
-                }
-                case 6 -> {
-                    totalCompra += precioUnitario * cantidad;
-                    iva = subtotal * 0.16;
-                    totalFactura = subtotal + iva;
-                    System.out.println("****** FACTURA DE COMPRA ******");
-                    System.out.println("Tienda Tecnológica");
-                    System.out.println("Nombre: " + nombre);
-                    System.out.println("Fecha: " + fecha);
-                    System.out.println("Teléfono: " + tel);
-                    System.out.println("Detalle de compra:");
-                    System.out.println("Producto\tCantidad\tValor Unitario\tTotal");
-                    System.out.println("-----------------------------------------------");
 
-                    if (producto.equals("Mause")) {
-                        System.out.println("Mause" + cantidad + "\t\t" + 80000 + "\t\t" + (80000 * cantidad));
+                case 6, 7 -> {
+                    System.out.print("Ganas experiencia en la aventura ");
+                    experiencia += 3;
+                    System.out.println("Vida: " + vida + " | Experiencia: " + experiencia);
+                }
+
+                case 8, 9 -> {
+                    System.out.print("Cometes un error grave ");
+                    experiencia -= 6;
+                    System.out.println("Vida: " + vida + " | Experiencia: " + experiencia);
+                }
+
+                case 10, 11 -> {
+                    System.out.print("Caes en una trampa letal  ");
+                    vida -= 8;
+                    System.out.println("Vida: " + vida + " | Experiencia: " + experiencia);
+                }
+
+                case 12 -> {
+                    int eventoEspecial = ale.nextInt(3) + 1;
+                    System.out.print("Evento especial aleatorio ");
+                    System.out.print(" Puede ser bueno o malo ");
+                    switch (eventoEspecial) {
+                        case 1 -> {
+                            System.out.print("Encuentras un pergamino antiguo ");
+                            experiencia += 10;
+                            System.out.println("Vida: " + vida + " | Experiencia: " + experiencia);
+                        }
+
+                        case 2 -> {
+                            System.out.print("Te infectas con veneno,  Empiezas a perder -2 puntos de vida por turno ");
+                            envenenado = true;
+                            System.out.println("Vida: " + vida + " | Experiencia: " + experiencia);
+                        }
+                        case 3 -> {
+                            System.out.println("Un demonio te ataca. Tu vida se reduce a la mitad.");
+                            vida /= 2;
+                            System.out.println("Vida: " + vida + " | Experiencia: " + experiencia);
+                        }
                     }
-                    if (producto.equals("Teclado")) {
-                        System.out.println("Teclado\t" + cantidad + "\t\t" + 320000 + "\t\t" + (320000 * cantidad));
+                    if (vida <= 0) {
+                        System.out.println("Has muerto. Fin del juego.");
+
+                    } else if (experiencia >= 50) {
+                        System.out.println("¡Has alcanzado 50 puntos de experiencia y escapaste de la dimensión! ¡Ganaste!");
+
                     }
-                    if (producto.equals("Monitor")) {
-                        System.out.println("Monitor\t" + cantidad + "\t\t" + 940000 + "\t\t" + (940000 * cantidad));
-                    }
-                    if (producto.equals("Ram")) {
-                        System.out.println("Ram\t" + cantidad + "\t\t" + 180000 + "\t\t" + (180000 * cantidad));
-                    }
-                    System.out.println("\nSubtotal: " + subtotal);
-                    System.out.println("IVA (16%): " + iva);
-                    System.out.println("Total de la compra: " + totalFactura);
-                    System.out.println("\nGracias por su compra.");
+
                 }
             }
 
-            if (opcion >= 1 && opcion <= 5) {
-                System.out.print("Ingrese la cantidad de " + producto + " a comprar: ");
-                cantidad = teclado.nextInt();
-                subtotal += precioUnitario * cantidad;
-                System.out.println("Producto: " + producto);
-                System.out.println("Cantidad: " + cantidad);
-                System.out.println("Valor Unitario: " + precioUnitario);
-                System.out.println("Total del producto: " + (precioUnitario * cantidad));
-
-
-            }
-
-            System.out.println("Producto: " + producto);
-            System.out.println("Cantidad: " + cantidad);
-            System.out.println("Valor Unitario: " + precioUnitario);
-            System.out.println("Total del producto: " + (precioUnitario * cantidad));
-
-        } while (opcion != 6);
+        }
     }
 }
